a0203f868ee5d30ccdfa8aa163afd459073ddb94
Varun Malaviya <varunmalaviya7@gmail.com>
2023-05-02 16:10:07 -0400

    added a showcase component as main intro to website

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
-import NavbarComponent from './components/NavbarComponent';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Showcase from './components/Showcase';
+import NavbarComponent from './components/NavbarComponent';
 
 function App() {
   return (
     <div className="App">
       <NavbarComponent />
+      <Showcase />
     </div>
   );
 }
---
+++ src/components/Showcase.js
@@ -0,0 +1,22 @@
+//the first section of the website seen bby users
+
+import { Button, Container } from "react-bootstrap";
+
+function Showcase(){
+    return(
+        <section className="bg-dark text-light p-5 text-center text-sm-start" >
+            <Container>
+                <div className="d-md-flex align-items-center justify-content-between">
+                    <div>
+                        <h1>Let Your Money <span class="text-success">Make You Money</span></h1>
+                        <p className="lead my-4">Each investor has a different story, and we are steadfast partners to our clients in the US because we listen to every one of them. Our full range of funds is one way we’re helping more investors build solid financial futures.</p>
+                        <Button variant="primary" size="lg">Contact The Hogs</Button>
+                    </div>
+                    <img className="img-fluid w-50 d-none d-sm-block" src="../images/stocks.jpg" alt="" />
+                </div>
+            </Container>
+        </section>
+    )
+}
+
+export default Showcase

ff53b92ae40fef8a1905410c6a264259ef485c35
Varun Malaviya <varunmalaviya7@gmail.com>
2023-05-04 16:21:21 -0400

    removed react icons in favor of font awesome library

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,14 @@
-import { Container } from "react-bootstrap"
+import { Container } from "react-bootstrap" 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowUp } from "@fortawesome/free-solid-svg-icons"
 
 function Footer(){
     return(
-        <footer className="p-5 bg-success text-white text-center position-relative">
+        <footer className="p-5 bg-dark text-white text-center position-relative">
             <Container>
                 <p className="lead">Copyright &copy; Varun Malaviya 2023</p>
                 <a href="#" className="position-absolute bottom-0 end-0 p-5">
-                    <i className="bi bi-arrow-up-circle h1 color-primary" style={{background:'blue'}}></i>
+                    <FontAwesomeIcon icon={faArrowUp} style={{height:"40px", color: "#1c71d8",}} />
                 </a>
             </Container>
         </footer>
